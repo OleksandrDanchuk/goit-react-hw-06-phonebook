22a149a7a1b1115070b031ad2a2a535b156a3f53
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-09-27 22:53:13 +0300

    total finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,16 @@
-import { useEffect, useState } from 'react';
 import css from './App.module.css';
 import { FormAddContacts } from './FormAddContacts/FormAddContacts';
 import { Filter } from './Filter/Filter';
 import { ListContacts } from './ListContacts/ListContacts';
-import { getLocalStorage, saveLocalStorage } from 'checkLocalStorage';
-const KEY_LOCAL_CONTACTS = 'cotacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setfilter] = useState('');
-
-  useEffect(() => {
-    const stateLocalStorage = getLocalStorage(KEY_LOCAL_CONTACTS);
-    if (stateLocalStorage) {
-      setContacts(stateLocalStorage);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (contacts.length === 0) {
-      localStorage.removeItem(KEY_LOCAL_CONTACTS);
-      return;
-    } else {
-      saveLocalStorage(KEY_LOCAL_CONTACTS, contacts);
-    }
-  }, [contacts]);
-
-  const handleChange = ({ target: { value } }) => {
-    setfilter(value.trimLeft());
-  };
-
-  const deleteContact = ({ target: { id } }) => {
-    setContacts(prevState => prevState.filter(obj => obj.id !== id));
-  };
-
-  const addContact = objContact => {
-    if (checkName(objContact)) {
-      setContacts(prevState => [...prevState, objContact]);
-    }
-  };
-
-  const checkName = ({ name, number }) => {
-    const resultCheck = contacts.find(
-      obj =>
-        obj.name.toLowerCase() === name.toLowerCase() || obj.number === number
-    );
-    if (resultCheck) {
-      alert(`${name} or  number: ${number} is already in contacts.`);
-    }
-    return !resultCheck;
-  };
-
-  const filterContacts = () => {
-    if (!filter) return contacts;
-    return contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(filter.toLowerCase());
-    });
-  };
-
   return (
     <div className={css.container}>
       <h2>Phonebook</h2>
-      <FormAddContacts addContact={addContact} />
+      <FormAddContacts />
       <h2>Contacts</h2>
-      <Filter handleChange={handleChange} value={filter} />
-      <ListContacts
-        filterRender={filterContacts()}
-        deleteContact={deleteContact}
-      />
+      <Filter />
+      <ListContacts />
     </div>
   );
 };
--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -1,27 +1,20 @@
-import { useState } from 'react';
 import css from './FormAddContacts.module.css';
 import { useDispatch } from 'react-redux';
 import { addContact } from 'components/redux/contactSlice';
 
 export const FormAddContacts = () => {
   const dispatch = useDispatch();
-  const [state, setState] = useState({
-    name: '',
-    number: '',
-  });
-
-  const handleChange = ({ target: { name, value } }) => {
-    setState(prevState => ({ ...prevState, [name]: value }));
-  };
 
   const submitAddContact = evt => {
     evt.preventDefault();
-    if (!state.name.trim() || !state.number.trim()) {
-      alert('Please enter the correct values');
-      return;
-    }
-    dispatch(addContact(state.name, state.number));
-    setState({ name: '', number: '' });
+    const form = evt.currentTarget.elements;
+
+    const name = form.name.value;
+    const number = form.number.value;
+
+    dispatch(addContact(name, number));
+
+    evt.currentTarget.reset();
   };
 
   return (
@@ -36,8 +29,6 @@ export const FormAddContacts = () => {
             pattern="^[a-zA-Z\s]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={handleChange}
-            value={state.name}
           />
         </label>
         <label className={css.label}>
@@ -49,8 +40,6 @@ export const FormAddContacts = () => {
             pattern="^[0-9]+$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            onChange={handleChange}
-            value={state.number}
           />
         </label>
         <button className={css.form_button}>Add contact</button>
